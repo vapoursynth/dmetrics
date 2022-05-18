cac309b6b1400a9bb635e0fe068a09d72559ef82
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2022-05-18 16:42:11 +0200

    Use more suitable types for everything Reduce the number of of pitch variables to only 1 instead of 7(?) or so

--- src/dmetrics.cpp
+++ src/dmetrics.cpp
@@ -1,9 +1,3 @@
-//////////////////////////////////////////
-// This file contains a simple filter
-// skeleton you can use to get started.
-// With no changes it simply passes
-// frames through.
-
 #include <VapourSynth4.h>
 #include <VSHelper4.h>
 
@@ -22,31 +16,29 @@ struct DMetricsSettings {
 };
 
 struct DMetricsData {
-    VSNode *node;
-    const VSVideoInfo *vi;
+    VSNode *node = nullptr;
+    const VSVideoInfo *vi = nullptr;
 	std::string mmprop;
 	std::string vmprop;
 	DMetricsSettings settings = {};
 };
 
 struct DMetricsSet {
-	int c;
-	int p;
-	int highest_sumc;
-	int highest_sump;
+	unsigned int c;
+	unsigned int p;
+	unsigned int highest_sumc;
+	unsigned int highest_sump;
 };
 
 static void calculateTelecideMetrics(const VSFrame *frameC, const VSFrame *frameP, const DMetricsSettings &settings, DMetricsSet &metrics, const VSAPI *vsapi) {
 	const bool tff = settings.tff;
 	const bool chroma = settings.chroma;
 	const unsigned int nt = settings.nt;
-	const int y0 = settings.y0;
-	const int y1 = settings.y1;
+	const size_t y0 = settings.y0;
+	const size_t y1 = settings.y1;
 
-	ptrdiff_t pitchc = vsapi->getStride(frameC, 0);
-	ptrdiff_t pitchp = pitchc;
-	ptrdiff_t pitchcUV = vsapi->getStride(frameC, 1);
-	ptrdiff_t pitchpUV = pitchcUV;
+	ptrdiff_t pitch = vsapi->getStride(frameC, 0);
+	ptrdiff_t pitchUV = vsapi->getStride(frameC, 1);
 
 	const uint8_t *fcrp = vsapi->getReadPtr(frameC, 0);
 	const uint8_t *fcrpU = vsapi->getReadPtr(frameC, 1);
@@ -56,97 +48,82 @@ static void calculateTelecideMetrics(const VSFrame *frameC, const VSFrame *frame
 	const uint8_t *fprpU = vsapi->getReadPtr(frameP, 1);
 	const uint8_t *fprpV = vsapi->getReadPtr(frameP, 2);
 
-	int w = vsapi->getFrameWidth(frameC, 0);
-	int h = vsapi->getFrameHeight(frameC, 0);
-	int wover2 = w / 2;
-	int hover2 = h / 2;
-	int xblocks = (w + BLKSIZE - 1) / BLKSIZE;
-	int yblocks = (h + BLKSIZE - 1) / BLKSIZE;
+	const size_t w = vsapi->getFrameWidth(frameC, 0);
+	const size_t h = vsapi->getFrameHeight(frameC, 0);
+	const size_t wover2 = w / 2;
+	const size_t hover2 = h / 2;
+	const size_t xblocks = (w + BLKSIZE - 1) / BLKSIZE;
+	const size_t yblocks = (h + BLKSIZE - 1) / BLKSIZE;
 
-	int x, y, p, c, tmp1, tmp2;
-	bool vc;
-	const unsigned char *currbot0, *currbot2, *prevbot0, *prevbot2;
-	const unsigned char *prevtop0, *prevtop2, *prevtop4, *currtop0, *currtop2, *currtop4;
-	const unsigned char *a0, *a2, *b0, *b2, *b4;
-	unsigned int diff, index;
-	ptrdiff_t apitchtimes4, bpitchtimes4, cpitchtimes4;
-	unsigned int highest_sumc, highest_sump;
-
-	constexpr size_t T = 4;
+	constexpr int T = 4;
 
 	size_t numblocks = xblocks * yblocks;
 
 	unsigned int *sumc = new unsigned int[numblocks]();
 	unsigned int *sump = new unsigned int[numblocks]();
 
-	/* Clear the block sums. */
-	/*
-	for (y = 0; y < yblocks; y++) {
-		for (x = 0; x < xblocks; x++) {
-			sump[y * xblocks + x] = 0;
-			sumc[y * xblocks + x] = 0;
-		}
-	}
-	*/
-
 	/* Find the best field match. Subsample the frames for speed. */
-	currbot0 = fcrp + pitchc;
-	currbot2 = fcrp + 3 * pitchc;
-	currtop0 = fcrp;
-	currtop2 = fcrp + 2 * pitchc;
-	cpitchtimes4 = 4 * pitchc;
-	currtop4 = fcrp + 4 * pitchc;
-	prevbot0 = fprp + pitchp;
-	prevbot2 = fprp + 3 * pitchp;
-	prevtop0 = fprp;
-	prevtop2 = fprp + 2 * pitchp;
-	prevtop4 = fprp + 4 * pitchp;
+	const uint8_t *currbot0 = fcrp + pitch;
+	const uint8_t *currbot2 = fcrp + 3 * pitch;
+	const uint8_t *currtop0 = fcrp;
+	const uint8_t *currtop2 = fcrp + 2 * pitch;
+	const uint8_t *currtop4 = fcrp + 4 * pitch;
+	const uint8_t *prevbot0 = fprp + pitch;
+	const uint8_t *prevbot2 = fprp + 3 * pitch;
+	const uint8_t *prevtop0 = fprp;
+	const uint8_t *prevtop2 = fprp + 2 * pitch;
+	const uint8_t *prevtop4 = fprp + 4 * pitch;
+
+	const uint8_t *a0;
+	const uint8_t *a2;
+	const uint8_t *b0;
+	const uint8_t *b2;
+	const uint8_t *b4;
+
 	if (tff) {
-		apitchtimes4 = 4 * pitchp;
 		a0 = prevbot0;
 		a2 = prevbot2;
-		bpitchtimes4 = 4 * pitchc;
 		b0 = currtop0;
 		b2 = currtop2;
 		b4 = currtop4;
 	} else {
-		apitchtimes4 = 4 * pitchc;
 		a0 = currbot0;
 		a2 = currbot2;
-		bpitchtimes4 = 4 * pitchp;
 		b0 = prevtop0;
 		b2 = prevtop2;
 		b4 = prevtop4;
 	}
-	p = c = 0;
+
+	unsigned int c = 0;
+	unsigned int p = 0;
 
 	// Calculate the field match and film/video metrics.
 	constexpr size_t skip = 1;
-	for (y = 0, index = 0; y < h - 4; y += 4) {
+	for (size_t y = 0; y < h - 4; y += 4) {
 		/* Exclusion band. Good for ignoring subtitles. */
 		if (y0 == y1 || y < y0 || y > y1) {
-			for (x = 0; x < w;) {
-				index = (y / BLKSIZE) * xblocks + x / BLKSIZE;
+			for (size_t x = 0; x < w;) {
+				size_t index = (y / BLKSIZE) * xblocks + x / BLKSIZE;
 
 				// Test combination with current frame.
-				tmp1 = ((long)currbot0[x] + (long)currbot2[x]);
+				int tmp1 = ((int)currbot0[x] + (int)currbot2[x]);
 				//				diff = abs((long)currtop0[x] - (tmp1 >> 1));
-				diff = abs((((long)currtop0[x] + (long)currtop2[x] + (long)currtop4[x])) - (tmp1 >> 1) - tmp1);
+				unsigned int diff = abs((((int)currtop0[x] + (int)currtop2[x] + (int)currtop4[x])) - (tmp1 >> 1) - tmp1);
 				if (diff > nt) {
 					c += diff;
 				}
 
 				tmp1 = currbot0[x] + T;
-				tmp2 = currbot0[x] - T;
-				vc = (tmp1 < currtop0[x] && tmp1 < currtop2[x]) ||
+				int tmp2 = currbot0[x] - T;
+				bool vc = (tmp1 < currtop0[x] && tmp1 < currtop2[x]) ||
 					(tmp2 > currtop0[x] && tmp2 > currtop2[x]);
 				if (vc) {
 					sumc[index]++;
 				}
 
 				// Test combination with previous frame.
-				tmp1 = ((long)a0[x] + (long)a2[x]);
-				diff = abs((((long)b0[x] + (long)b2[x] + (long)b4[x])) - (tmp1 >> 1) - tmp1);
+				tmp1 = ((int)a0[x] + (int)a2[x]);
+				diff = abs((((int)b0[x] + (int)b2[x] + (int)b4[x])) - (tmp1 >> 1) - tmp1);
 				if (diff > nt) {
 					p += diff;
 				}
@@ -163,87 +140,81 @@ static void calculateTelecideMetrics(const VSFrame *frameC, const VSFrame *frame
 				if (!(x & 3)) x += 4;
 			}
 		}
-		currbot0 += cpitchtimes4;
-		currbot2 += cpitchtimes4;
-		currtop0 += cpitchtimes4;
-		currtop2 += cpitchtimes4;
-		currtop4 += cpitchtimes4;
-		a0 += apitchtimes4;
-		a2 += apitchtimes4;
-		b0 += bpitchtimes4;
-		b2 += bpitchtimes4;
-		b4 += bpitchtimes4;
+		currbot0 += 4 * pitch;
+		currbot2 += 4 * pitch;
+		currtop0 += 4 * pitch;
+		currtop2 += 4 * pitch;
+		currtop4 += 4 * pitch;
+		a0 += 4 * pitch;
+		a2 += 4 * pitch;
+		b0 += 4 * pitch;
+		b2 += 4 * pitch;
+		b4 += 4 * pitch;
 	}
 
 	if (chroma) {
-		cpitchtimes4 = 4 * pitchcUV;
-
 		for (int z = 0; z < 2; z++) {
 			// Do the same for the U plane.
 			if (z == 0) {
-				currbot0 = fcrpU + pitchcUV;
-				currbot2 = fcrpU + 3 * pitchcUV;
+				currbot0 = fcrpU + pitchUV;
+				currbot2 = fcrpU + 3 * pitchUV;
 				currtop0 = fcrpU;
-				currtop2 = fcrpU + 2 * pitchcUV;
-				currtop4 = fcrpU + 4 * pitchcUV;
-				prevbot0 = fprpU + pitchpUV;
-				prevbot2 = fprpU + 3 * pitchpUV;
+				currtop2 = fcrpU + 2 * pitchUV;
+				currtop4 = fcrpU + 4 * pitchUV;
+				prevbot0 = fprpU + pitchUV;
+				prevbot2 = fprpU + 3 * pitchUV;
 				prevtop0 = fprpU;
-				prevtop2 = fprpU + 2 * pitchpUV;
-				prevtop4 = fprpU + 4 * pitchpUV;
+				prevtop2 = fprpU + 2 * pitchUV;
+				prevtop4 = fprpU + 4 * pitchUV;
 			} else {
-				currbot0 = fcrpV + pitchcUV;
-				currbot2 = fcrpV + 3 * pitchcUV;
+				currbot0 = fcrpV + pitchUV;
+				currbot2 = fcrpV + 3 * pitchUV;
 				currtop0 = fcrpV;
-				currtop2 = fcrpV + 2 * pitchcUV;
-				currtop4 = fcrpV + 4 * pitchcUV;
-				prevbot0 = fprpV + pitchpUV;
-				prevbot2 = fprpV + 3 * pitchpUV;
+				currtop2 = fcrpV + 2 * pitchUV;
+				currtop4 = fcrpV + 4 * pitchUV;
+				prevbot0 = fprpV + pitchUV;
+				prevbot2 = fprpV + 3 * pitchUV;
 				prevtop0 = fprpV;
-				prevtop2 = fprpV + 2 * pitchpUV;
-				prevtop4 = fprpV + 4 * pitchpUV;
+				prevtop2 = fprpV + 2 * pitchUV;
+				prevtop4 = fprpV + 4 * pitchUV;
 			}
 			if (tff) {
-				apitchtimes4 = 4 * pitchpUV;
 				a0 = prevbot0;
 				a2 = prevbot2;
-				bpitchtimes4 = 4 * pitchcUV;
 				b0 = currtop0;
 				b2 = currtop2;
 				b4 = currtop4;
 			} else {
-				apitchtimes4 = 4 * pitchcUV;
 				a0 = currbot0;
 				a2 = currbot2;
-				bpitchtimes4 = 4 * pitchpUV;
 				b0 = prevtop0;
 				b2 = prevtop2;
 				b4 = prevtop4;
 			}
 
-			for (y = 0; y < hover2 - 4; y += 4) {
+			for (size_t y = 0; y < hover2 - 4; y += 4) {
 				/* Exclusion band. Good for ignoring subtitles. */
 				if (y0 == y1 || y < y0 / 2 || y > y1 / 2) {
-					for (x = 0; x < wover2;) {
-						index = (y / BLKSIZE) * xblocks + x / BLKSIZE;
+					for (size_t x = 0; x < wover2;) {
+						size_t index = (y / BLKSIZE) * xblocks + x / BLKSIZE;
 						// Test combination with current frame.
-						tmp1 = ((long)currbot0[x] + (long)currbot2[x]);
-						diff = abs((((long)currtop0[x] + (long)currtop2[x] + (long)currtop4[x])) - (tmp1 >> 1) - tmp1);
+						int tmp1 = ((int)currbot0[x] + (int)currbot2[x]);
+						unsigned int diff = abs((((int)currtop0[x] + (int)currtop2[x] + (int)currtop4[x])) - (tmp1 >> 1) - tmp1);
 						if (diff > nt) {
 							c += diff;
 						}
 
 						tmp1 = currbot0[x] + T;
-						tmp2 = currbot0[x] - T;
-						vc = (tmp1 < currtop0[x] && tmp1 < currtop2[x]) ||
+						int tmp2 = currbot0[x] - T;
+						bool vc = (tmp1 < currtop0[x] && tmp1 < currtop2[x]) ||
 							(tmp2 > currtop0[x] && tmp2 > currtop2[x]);
 						if (vc) {
 							sumc[index]++;
 						}
 
 						// Test combination with previous frame.
-						tmp1 = ((long)a0[x] + (long)a2[x]);
-						diff = abs((((long)b0[x] + (long)b2[x] + (long)b4[x])) - (tmp1 >> 1) - tmp1);
+						tmp1 = ((int)a0[x] + (int)a2[x]);
+						diff = abs((((int)b0[x] + (int)b2[x] + (int)b4[x])) - (tmp1 >> 1) - tmp1);
 						if (diff > nt) {
 							p += diff;
 						}
@@ -260,37 +231,26 @@ static void calculateTelecideMetrics(const VSFrame *frameC, const VSFrame *frame
 						if (!(x & 3)) x += 4;
 					}
 				}
-				currbot0 += cpitchtimes4;
-				currbot2 += cpitchtimes4;
-				currtop0 += cpitchtimes4;
-				currtop2 += cpitchtimes4;
-				currtop4 += cpitchtimes4;
-				a0 += apitchtimes4;
-				a2 += apitchtimes4;
-				b0 += bpitchtimes4;
-				b2 += bpitchtimes4;
-				b4 += bpitchtimes4;
+				currbot0 += 4 * pitchUV;
+				currbot2 += 4 * pitchUV;
+				currtop0 += 4 * pitchUV;
+				currtop2 += 4 * pitchUV;
+				currtop4 += 4 * pitchUV;
+				a0 += 4 * pitchUV;
+				a2 += 4 * pitchUV;
+				b0 += 4 * pitchUV;
+				b2 += 4 * pitchUV;
+				b4 += 4 * pitchUV;
 			}
 		}
 	}
 
-	if (true /*post != POST_NONE*/) {
-		highest_sump = 0;
-		for (y = 0; y < yblocks; y++) {
-			for (x = 0; x < xblocks; x++) {
-				if (sump[y * xblocks + x] > highest_sump) {
-					highest_sump = sump[y * xblocks + x];
-				}
-			}
-		}
-		highest_sumc = 0;
-		for (y = 0; y < yblocks; y++) {
-			for (x = 0; x < xblocks; x++) {
-				if (sumc[y * xblocks + x] > highest_sumc) {
-					highest_sumc = sumc[y * xblocks + x];
-				}
-			}
-		}
+	unsigned int highest_sumc = 0;
+	unsigned int highest_sump = 0;
+
+	for (size_t i = 0; i < numblocks; i++) {
+		highest_sump = std::max(highest_sump, sump[i]);
+		highest_sumc = std::max(highest_sumc, sumc[i]);
 	}
 
 	delete [] sumc;
